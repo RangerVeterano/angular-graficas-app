c89d81eeec0c36bdfa6cdcf3825d868f3893a985
Ignacio Bohigues <harvestveterano@gmail.com>
2022-01-24 18:32:54 +0100

    Implementacion de todas las rutas

--- src/app/graficas/graficas-routing.module.ts
+++ src/app/graficas/graficas-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { BarrasDonaComponent } from './pages/barras-dona/barras-dona.component';
+import { BarrasDonaHttpComponent } from './pages/barras-dona-http/barras-dona-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+       path: 'barra',
+       component: BarrasComponent
+      },
+      {
+        path: 'barra-doble',
+        component: BarrasDobleComponent
+      },
+      {
+        path: 'dona',
+        component: BarrasDonaComponent
+      },
+      {
+        path: 'dona-http',
+        component: BarrasDonaHttpComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'barra'
+      }
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
